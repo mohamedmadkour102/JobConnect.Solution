15ec3030abc4934368f1e1c3c73901dd9c1b58c8
Mohamed Madkour <Mohamed.Madkour2002@gmail.com>
2024-12-04 15:26:20 +0200

    Add project files.

---
+++ JobConnect.Apis/Program.cs
@@ -0,0 +1,68 @@
+using JobConnect.Core.Models;
+using JobConnect.Repository.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+#region DI
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}); 
+#endregion
+
+#region Identity
+builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddAuthentication(); 
+#endregion
+
+var app = builder.Build();
+#region Migration
+
+using var Scope = app.Services.CreateScope();
+var Services = Scope.ServiceProvider;
+// Ask CLR for creating object from dbcontext explicitly
+var _dbContext = Services.GetRequiredService<AppDbContext>();
+
+var LoggerFactory = Services.GetRequiredService<ILoggerFactory>();
+
+try
+{
+	await _dbContext.Database.MigrateAsync();
+	var UserManager = Services.GetRequiredService<UserManager<User>>();
+	await AppDbContextSeed.SeedUserAsync(UserManager);
+}
+catch (Exception ex)
+{
+	var Logger = LoggerFactory.CreateLogger<Program>();
+	Logger.LogError(ex, "An error occurred while applying the migration");
+}
+#endregion
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ JobConnect.Repository/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using JobConnect.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobConnect.Repository.Data
+{
+	public class AppDbContext : IdentityDbContext<User>
+	{
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+
+	}
+}
