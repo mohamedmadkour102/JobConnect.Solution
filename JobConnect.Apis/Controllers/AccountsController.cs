b1847657ea558ff4d0d9db4cc6d2f83f6880dae5
Mohamed Madkour <Mohamed.Madkour2002@gmail.com>
2024-12-05 02:22:40 +0200

    Project Structre and Connecting with Db Context

--- JobConnect.Apis/Controllers/AccountsController.cs
+++ JobConnect.Apis/Controllers/AccountsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JobConnect.Apis.DTO_s;
+using JobConnect.Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JobConnect.Apis.Controllers
@@ -7,6 +10,35 @@ namespace JobConnect.Apis.Controllers
 	[ApiController]
 	public class AccountsController : ControllerBase
 	{
-		
+		private readonly UserManager<User> _userManager;
+
+		public AccountsController(UserManager<User> userManager ) 
+		{
+			_userManager = userManager;
+		}
+
+		[HttpPost("Register")]
+		public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+		{
+			var User = new User()
+			{
+				FirstName = registerDto.FirstName,
+				LastName = registerDto.LastName,
+				Email = registerDto.Email,
+				PhoneNumber = registerDto.PhoneNumber,
+				UserName = registerDto.Email.Split('@')[0]
+			};
+			var result = await _userManager.CreateAsync(User, registerDto.Password);
+			if (!result.Succeeded) return BadRequest(result);
+			var ReturnedUser = new UserDto() 
+			{
+				Name = $"{registerDto.FirstName} {registerDto.LastName}",
+				Email = registerDto.Email,
+				Token = "This is Token"
+			};
+			return Ok(ReturnedUser);
+			}
+
+
 	}
 }
