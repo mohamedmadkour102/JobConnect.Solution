4c090193a25d8a3c6d2c5820f8a1f6687a9b9ce6
Mohamed Madkour <Mohamed.Madkour2002@gmail.com>
2024-12-08 07:13:15 +0200

    ForgotPassword and ResetPassword

---
+++ JobConnect.Apis/DTO's/ResetPasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JobConnect.Apis.DTO_s
+{
+	public class ResetPasswordDto
+	{
+		public string Email { get; set; }
+		public string Token { get; set; }
+		public string NewPassword { get; set; }
+	}
+}
