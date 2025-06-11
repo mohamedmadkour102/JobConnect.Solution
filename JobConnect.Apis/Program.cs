623f8ff7d4cbbff259686d1e662c145174efd1b8
GamallAhmed <gimyahmed028@gmail.com>
2025-06-11 20:48:24 +0300

    string.empty

--- JobConnect.Apis/Program.cs
+++ JobConnect.Apis/Program.cs
@@ -189,8 +189,8 @@ app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "JobConnect API V1");
-   // c.RoutePrefix = string.Empty;
-    c.RoutePrefix = "Swagger";
+    c.RoutePrefix = string.Empty;
+    //c.RoutePrefix = "Swagger";
 
 });
 
